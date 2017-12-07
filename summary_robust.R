34a97bba96442b1d4c68deb5cc87a0b75bb60c2a
Adam <adamlu1128@gmail.com>
2017-12-06 16:05:02 -0800

    Update summary_robust.R

--- summary_robust.R
+++ summary_robust.R
@@ -5,8 +5,6 @@ summary.robust <- function(model) {
   u2 <- residuals(model)^2
   XDX <- 0
   
-  ## Here one needs to calculate X'DX. But due to the fact that
-  ## D is huge (NxN), it is better to do it with a cycle.
   for(i in 1:nrow(X)) {
     XDX <- XDX + u2[i]*X[i,]%*%t(X[i,])
   }
@@ -14,7 +12,7 @@ summary.robust <- function(model) {
   # inverse(X'X)
   XX1 <- solve(t(X)%*%X)
   
-  # Variance calculation (Bread x meat x Bread)
+  # Variance calculation 
   varcovar <- XX1 %*% XDX %*% XX1
   
   # degrees of freedom adjustment
